db6c9704eb205580a19aa71740ff216ef6c46da2
Ioannis <ioannis-karatsimakidis@hotmail.de>
2021-01-02 16:06:33 +0100

    fixed SA

--- run.py
+++ run.py
@@ -16,17 +16,18 @@ from scrapy.crawler import CrawlerRunner
 from scrapy.utils.log import configure_logging
 from scrapy.utils.project import get_project_settings
 
-from jedeschule.spiders.bayern import BayernSpider
-from jedeschule.spiders.bremen import BremenSpider
-from jedeschule.spiders.brandenburg import BrandenburgSpider
-from jedeschule.spiders.hamburg import HamburgSpider
-from jedeschule.spiders.niedersachsen import NiedersachsenSpider
-from jedeschule.spiders.sachsen import SachsenSpider
-from jedeschule.spiders.sachsen_anhalt import SachsenAnhaltSpider
-from jedeschule.spiders.thueringen import ThueringenSpider
-from jedeschule.spiders.schleswig_holstein import SchleswigHolsteinSpider
-from jedeschule.spiders.berlin import BerlinSpider
-from jedeschule.spiders.rheinland_pfalz import RheinlandPfalzSpider
+#from jedeschule.spiders.bayern import BayernSpider
+#from jedeschule.spiders.bremen import BremenSpider
+#from jedeschule.spiders.brandenburg import BrandenburgSpider
+#from jedeschule.spiders.hamburg import HamburgSpider
+#from jedeschule.spiders.niedersachsen import NiedersachsenSpider
+#from jedeschule.spiders.sachsen import SachsenSpider
+#from jedeschule.spiders.sachsen_anhalt import SachsenAnhaltSpider
+#from jedeschule.spiders.thueringen import ThueringenSpider
+#from jedeschule.spiders.schleswig_holstein import SchleswigHolsteinSpider
+#from jedeschule.spiders.berlin import BerlinSpider
+#from jedeschule.spiders.rheinland_pfalz import RheinlandPfalzSpider
+from jedeschule.spiders.saarland import SaarlandSpider
 
 configure_logging()
 settings = get_project_settings()
@@ -191,22 +192,23 @@ def get_nrw():
 
 @defer.inlineCallbacks
 def crawl():
-    yield runner.crawl(BremenSpider)
-    yield runner.crawl(BrandenburgSpider)
-    yield runner.crawl(BayernSpider)
-    yield runner.crawl(HamburgSpider)
-    yield runner.crawl(NiedersachsenSpider)
-    yield runner.crawl(SachsenSpider)
-    yield runner.crawl(SachsenAnhaltSpider)
-    yield runner.crawl(ThueringenSpider)
-    yield runner.crawl(SchleswigHolsteinSpider)
-    yield runner.crawl(BerlinSpider)
-    yield runner.crawl(RheinlandPfalzSpider)
+    #yield runner.crawl(BremenSpider)
+    #yield runner.crawl(BrandenburgSpider)
+    #yield runner.crawl(BayernSpider)
+    #yield runner.crawl(HamburgSpider)
+    #yield runner.crawl(NiedersachsenSpider)
+    #yield runner.crawl(SachsenSpider)
+    #yield runner.crawl(SachsenAnhaltSpider)
+    #yield runner.crawl(ThueringenSpider)
+    #yield runner.crawl(SchleswigHolsteinSpider)
+    #yield runner.crawl(BerlinSpider)
+    #yield runner.crawl(RheinlandPfalzSpider)
+    yield runner.crawl(SaarlandSpider)
     reactor.stop()
 
 
 if __name__ == '__main__':
     crawl()
     reactor.run()  # the script will block here until the last crawl call is finished
-    get_mv()
-    get_nrw()
+    #get_mv()
+    #get_nrw()
